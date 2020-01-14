439c95abc57cf41886ce0f38de779df92e91ed6b
yukihira1992 <hirayama@cancerscan.jp>
2020-01-14 15:44:13 +0900

    Fixed version.

--- create-patch.sh
+++ create-patch.sh
@@ -3,15 +3,14 @@
 python main.py > date.txt
 
 if [ -n "$(git status --porcelain)" ]; then
-    LATEST_TAG=`git tag -l --sort=-v:refname | grep -E '^v\d+\.\d+\.\d+' | head -n 1`
     git remote set-url origin https://yukihira1992:${GITHUB_TOKEN}@github.com/yukihira1992/gha.git
     git config --local user.name "yukihira1992"
     git config --local user.email "ykhr0130@gmail.com"
-    echo $LATEST_TAG > ./tag.txt
+    python next_patch.py `cat version.txt` > version.txt
     git add date.txt
-    git add tag.txt
+    git add version.txt
     git commit -m "Updated date."
-    NEXT_TAG=`python next_patch.py $LATEST_TAG`
-    git tag v${NEXT_TAG}
+    NEW_VERSION=`cat version.txt`
+    git tag v${NEW_VERSION}
     git push origin HEAD --tags
 fi
