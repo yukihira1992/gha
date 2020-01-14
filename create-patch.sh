15f6b840dad31ed50724ec43166c147430d8af19
yukihira1992 <hirayama@cancerscan.jp>
2020-01-14 13:51:04 +0900

    Fixed condition.

--- create-patch.sh
+++ create-patch.sh
@@ -2,7 +2,7 @@
 
 python main.py > date.txt
 
-if [ -z "$(git status --porcelain)" ]; then
+if [ -n "$(git status --porcelain)" ]; then
     git remote set-url origin https://yukihira1992:${GITHUB_TOKEN}@github.com/yukihira1992/gha.git
     git config --local user.name "yukihira1992"
     git config --local user.email "ykhr0130@gmail.com"
@@ -10,6 +10,5 @@ if [ -z "$(git status --porcelain)" ]; then
     git commit -m "Updated date."
     TAG=`cat /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
     git tag $TAG
-    git push origin HEAD
-    git push origin $TAG
+    git push origin HEAD --tags
 fi
